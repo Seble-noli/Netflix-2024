b509d822495495e77eef9d05347b8eb04b3f9323
Seble-noli <seblewengel1227@gmail.com>
2025-12-08 09:09:19 -0800

    banner and rows

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from '../../componets/Header/Header'
 import Footer from '../../componets/Footer/Footer'
+import Banner from '../../componets/Banner/Banner'
+import RowList from '../../componets/Rows/RowList/RowList'
 
 const Home = () => {
   return (
     <>
     <Header/>
+    <Banner/>
+    <RowList/>
     <Footer/>
     </>
   )
--- netflix-clone/src/utils/requests.jsx
+++ netflix-clone/src/utils/requests.jsx
@@ -1,13 +1,16 @@
-const API_KEY = Process.env.REACT-APP-API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const requests = {
-  fetchTreading: `/treanding/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTreading: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRatedMovies: `/discover/top_rated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movies?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movies?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movies?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movies?api_key=${API_KEY}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movies?api_key=${API_KEY}&with_genres=99`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
   fetchTvShow:`tv/popular?api_key=${API_KEY}&language=en-US&page=1`
-};
+ };
+
+
+
+
 export default requests;
